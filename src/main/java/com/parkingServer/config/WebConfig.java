035ca94496e303611083fc98a3b122d62a92d8d6
Tejas <tejastpatil230@gmail.com>
2025-09-30 20:22:34 +0530

    Hosting CORS policy Updated

--- src/main/java/com/parkingServer/config/WebConfig.java
+++ src/main/java/com/parkingServer/config/WebConfig.java
@@ -7,14 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://staging.d242n84voexwpp.amplifyapp.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
             }
         };
     }
